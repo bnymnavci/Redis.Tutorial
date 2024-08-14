157c227727c685aba8dbb76ddaaaa1975b833d66
Bünyamin Avcı <67219504+bnymnavci@users.noreply.github.com>
2024-08-15 01:54:00 +0300

    Add Redis files

---
+++ Redis.Publisher/Program.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+
+
+
+
+
+
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:2727");
+ISubscriber subscriber = connection.GetSubscriber();
+
+while (true)
+{
+    Console.WriteLine("Message: ");
+    string message = Console.ReadLine();
+    await subscriber.PublishAsync("mychannel", message);
+}
---
+++ Redis.Subscriber/Program.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+
+
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:2727");
+ISubscriber subscriber = connection.GetSubscriber();
+
+subscriber.SubscribeAsync("mychannel", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+
+Console.Read();
